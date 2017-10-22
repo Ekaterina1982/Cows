c337d846bb12dc7cdd926a67e16610dcaa74305d
LocalUser <LocalUser@K3-512-06>
2017-10-22 15:08:58 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine (CowConter (45));
+            Console.ReadLine();
+        }
+
+        public static string CowConter(int n)
+        {
+            int lastDigit = n % 10;
+            int lastDigit2 = n % 100;
+            string result=string.Empty;
+
+            if ((lastDigit2 >= 11) & (lastDigit2 <= 19))
+                result = "коров";
+            else
+                if (lastDigit == 1) 
+                                    result = "корова";
+                else 
+                    if (lastDigit >= 2 && lastDigit <= 4)
+                                                          result = "коровы";
+                    else
+                        if ((lastDigit >= 5 && lastDigit <= 9) || (lastDigit == 0))
+                                                                                   result = "коров";
+                        
+              return string.Format("{0} {1}", n, result);
+
+        }
+
+    }
+
+
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication10;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 1;
+            string actual = Program.CowConter(n);
+
+            string expected = "1 корова";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            int n = 11;
+            string actual = Program.CowConter(n);
+
+            string expected = "11 коров";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod101()
+        {
+            int n = 101;
+            string actual = Program.CowConter(n);
+
+            string expected = "101 корова";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod111()
+        {
+            int n = 111;
+            string actual = Program.CowConter(n);
+
+            string expected = "111 коров";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod0()
+        {
+            int n = 1000;
+            string actual = Program.CowConter(n);
+
+            string expected = "1000 коров";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod112()
+        {
+            int n = 112;
+            string actual = Program.CowConter(n);
+
+            string expected = "112 коров";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod11155()
+        {
+            int n = 123;
+            string actual = Program.CowConter(n);
+
+            string expected = "123 коровы";
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod123()
+        {
+            int n = 11155;
+            string actual = Program.CowConter(n);
+
+            string expected = "11155 коров";
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5590()
+        {
+            int n = 5590;
+            string actual = Program.CowConter(n);
+
+            string expected = "5590 коров";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
